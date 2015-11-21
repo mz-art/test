3ee7df9d223f50e31553f06c38ce0929516c4d50
shy4blue <shy4blue@gmail.com>
2015-11-21 23:53:03 +0900

    attribute rounting 설정 했

--- Test/App_Start/RouteConfig.cs
+++ Test/App_Start/RouteConfig.cs
@@ -10,6 +10,8 @@ namespace Test {
         public static void RegisterRoutes(RouteCollection routes) {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.MapMvcAttributeRoutes();
+
             /*
             routes.MapRoute(
                 name: "Default",
